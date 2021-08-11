8fa87f7be0efd17082c57a678bf73c92447224d3
kemalgurler <kemgur52@gmail.com>
2021-08-11 18:58:19 -0400

    v4

--- src/FirstProject/linnbank.java
+++ src/FirstProject/linnbank.java
@@ -4,5 +4,6 @@ public class linnbank {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("We are learning java");
+        System.out.println("You can learn java");
     }
 }
